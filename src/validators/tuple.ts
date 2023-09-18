a3ffef30a2260dbddb8c72fd02b24ea25108e61f
DESKTOP-MD2B2EU\oyc <ouyangcheng@inmeng.net>
2023-09-18 17:29:48 +0800

    feat: 玩命开发中...

--- src/validators/tuple.ts
+++ src/validators/tuple.ts
@@ -1,7 +1,19 @@
 import { createValidator } from "../validator";
-import { AnyValidatorExpression } from "./any";
+import { BooleanValidatorExpression } from "./boolean";
+import { DateRangeValidatorExpression } from "./data-range";
+import { DateValidatorExpression } from "./date";
+import { NumberValidatorExpression } from "./number";
+import { NumberRangeValidatorExpression } from "./number-range";
+import { StringValidatorExpression } from "./string";
 
-export type TupleValidatorExpression = readonly AnyValidatorExpression[];
+export type TupleValidatorExpression = readonly (
+  | StringValidatorExpression
+  | NumberValidatorExpression
+  | NumberRangeValidatorExpression
+  | BooleanValidatorExpression
+  | DateValidatorExpression
+  | DateRangeValidatorExpression
+)[];
 
 export const tupleValidator = createValidator<TupleValidatorExpression, "tuple">({
   arrayMode: "tuple",
--- src/validators/union.ts
+++ src/validators/union.ts
@@ -1,7 +1,19 @@
 import { createValidator } from "../validator";
-import { AnyValidatorExpression } from "./any";
+import { BooleanValidatorExpression } from "./boolean";
+import { DateRangeValidatorExpression } from "./data-range";
+import { DateValidatorExpression } from "./date";
+import { NumberValidatorExpression } from "./number";
+import { NumberRangeValidatorExpression } from "./number-range";
+import { StringValidatorExpression } from "./string";
 
-export type UnionValidatorExpression = readonly AnyValidatorExpression[];
+export type UnionValidatorExpression = readonly (
+  | StringValidatorExpression
+  | NumberValidatorExpression
+  | NumberRangeValidatorExpression
+  | BooleanValidatorExpression
+  | DateValidatorExpression
+  | DateRangeValidatorExpression
+)[];
 
 export const unionValidator = createValidator<UnionValidatorExpression, "union">({
   arrayMode: "union",
