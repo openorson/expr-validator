364ec0ed7e8e5377e25cc5f363c315d641bf906f
DESKTOP-MD2B2EU\oyc <ouyangcheng@inmeng.net>
2023-09-18 18:06:46 +0800

    feat: 玩命开发中...

--- src/validators/object.ts
+++ src/validators/object.ts
@@ -1,9 +1,23 @@
 import { createValidator } from "../validator";
-import { AnyValidatorExpression } from "./any";
+import { BooleanValidatorExpression } from "./boolean";
+import { DateRangeValidatorExpression } from "./data-range";
+import { DateValidatorExpression } from "./date";
+import { NumberValidatorExpression } from "./number";
+import { NumberRangeValidatorExpression } from "./number-range";
+import { StringValidatorExpression } from "./string";
+import { TupleValidatorExpression } from "./tuple";
 
-type ObjectValidatorExpression<Expression = Record<string, AnyValidatorExpression> | AnyValidatorExpression> =
-  | Record<string, Expression>
-  | AnyValidatorExpression;
+type ObjectValidatorFieldExpression =
+  | StringValidatorExpression
+  | NumberValidatorExpression
+  | NumberRangeValidatorExpression
+  | BooleanValidatorExpression
+  | DateValidatorExpression
+  | DateRangeValidatorExpression
+  | TupleValidatorExpression;
+type ObjectValidatorExpression<
+  Expression = Record<string, ObjectValidatorFieldExpression> | ObjectValidatorFieldExpression
+> = Record<string, Expression> | ObjectValidatorFieldExpression;
 export type NestedObjectValidatorExpression = ObjectValidatorExpression<
   ObjectValidatorExpression<ObjectValidatorExpression<ObjectValidatorExpression<ObjectValidatorExpression>>>
 >;
