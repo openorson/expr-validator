273b291b1a8f1e4e1c0247769c6f7c0914eb22be
DESKTOP-MD2B2EU\oyc <ouyangcheng@inmeng.net>
2023-09-23 20:54:28 +0800

    feat: 玩命开发中...

--- src/validators/union.ts
+++ src/validators/union.ts
@@ -1,9 +1,9 @@
 import { ValidatorArrayExpressionUnionMode } from "../types/expression";
 import { createValidator } from "../validator/validator";
-import { BooleanValidatorExpression } from "./boolean";
-import { DateValidatorExpression } from "./date";
-import { NumberValidatorExpression } from "./number";
-import { StringValidatorExpression } from "./string";
+import { BooleanValidatorExpression, booleanValidator } from "./boolean";
+import { DateValidatorExpression, dateValidator } from "./date";
+import { NumberValidatorExpression, numberValidator } from "./number";
+import { StringValidatorExpression, stringValidator } from "./string";
 
 export type UnionValidatorExpression = readonly [
   ValidatorArrayExpressionUnionMode,
@@ -13,5 +13,35 @@ export type UnionValidatorExpression = readonly [
 export interface UnionValidatorOptions {}
 
 export const unionValidator = createValidator<UnionValidatorExpression, UnionValidatorOptions>({
-  validate(context) {},
+  validate({ expression, value, parse }) {
+    const [_1, ...unionExpression] = expression;
+    const [_2, ...unionParse] = parse;
+
+    let index = 0;
+    while (index < unionExpression.length) {
+      const expression = unionExpression[index];
+      const parse = unionParse[index];
+
+      if (parse.type === "string") {
+        const valid = stringValidator.$options.validate({ value, parse, expression: expression as StringValidatorExpression });
+        if (!valid) return;
+      }
+      if (parse.type === "number") {
+        const valid = numberValidator.$options.validate({ value, parse, expression: expression as NumberValidatorExpression });
+        if (!valid) return;
+      }
+      if (parse.type === "boolean") {
+        const valid = booleanValidator.$options.validate({ value, parse, expression: expression as BooleanValidatorExpression });
+        if (!valid) return;
+      }
+      if (parse.type === "date") {
+        const valid = dateValidator.$options.validate({ value, parse, expression: expression as DateValidatorExpression });
+        if (!valid) return;
+      }
+
+      index++;
+    }
+
+    return { type: "invalid" };
+  },
 });
--- tests/union.test.ts
+++ tests/union.test.ts
@@ -2,6 +2,6 @@ import { expect, test } from "vitest";
 import { validator } from "../src/index";
 
 test("union", () => {
-  const arr: unknown = ["a", 1];
-  expect(validator.union(arr, ["union", "string[]!", "number!"])).toBe(true);
+  const data: unknown = 1;
+  expect(validator.union(data, ["union", "string!", "number!"], { throw: true })).toBe(true);
 });
