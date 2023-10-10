d646d0e212f424dc6cd6eabbcd508c2beb4e7efa
DESKTOP-MD2B2EU\oyc <ouyangcheng@inmeng.net>
2023-10-10 16:23:36 +0800

    feat: 玩命开发中...

--- tests/number.test.ts
+++ tests/number.test.ts
@@ -1,7 +1,59 @@
 import { expect, test } from "vitest";
 import { validator } from "../src/index";
 
-test("number", () => {
-  const data: unknown = 1.5;
-  expect(validator.number(data, "number?{1~2}")).toBe(true);
+test("number type test", () => {
+  expect(validator.number(null, "number?")).toBe(true);
+  expect(validator.number(void 0, "number?")).toBe(true);
+  expect(validator.number(1, "number?")).toBe(true);
+  expect(validator.number("1", "number?")).toBe(false);
+
+  expect(validator.number(null, "number!")).toBe(false);
+  expect(validator.number(void 0, "number!")).toBe(false);
+  expect(validator.number(1, "number!")).toBe(true);
+  expect(validator.number("1", "number!")).toBe(false);
+
+  expect(validator.number(null, "number[]?")).toBe(true);
+  expect(validator.number(void 0, "number[]?")).toBe(true);
+  expect(validator.number([], "number[]?")).toBe(true);
+  expect(validator.number([1, 2, 3], "number[]?")).toBe(true);
+  expect(validator.number(["1", "2", "3"], "number[]?")).toBe(false);
+
+  expect(validator.number(null, "number[]!")).toBe(false);
+  expect(validator.number(void 0, "number[]!")).toBe(false);
+  expect(validator.number([], "number[]!")).toBe(true);
+  expect(validator.number([1, 2, 3], "number[]!")).toBe(true);
+  expect(validator.number(["1", "2", "3"], "number[]!")).toBe(false);
+});
+
+test("number size args test", () => {
+  expect(validator.number(9, "number!{10~}")).toBe(false);
+  expect(validator.number(10, "number!{10~}")).toBe(true);
+  expect(validator.number(11, "number!{10~}")).toBe(true);
+
+  expect(validator.number(9, "number!{~10}")).toBe(true);
+  expect(validator.number(10, "number!{~10}")).toBe(true);
+  expect(validator.number(11, "number!{~10}")).toBe(false);
+
+  expect(validator.number(9, "number!{10~20}")).toBe(false);
+  expect(validator.number(10, "number!{10~20}")).toBe(true);
+  expect(validator.number(15, "number!{10~20}")).toBe(true);
+  expect(validator.number(20, "number!{10~20}")).toBe(true);
+  expect(validator.number(21, "number!{10~20}")).toBe(false);
+});
+
+test("number length args test", () => {
+  expect(validator.number(1, "number!{3.}")).toBe(true);
+  expect(validator.number(10, "number!{3.}")).toBe(true);
+  expect(validator.number(100, "number!{3.}")).toBe(true);
+  expect(validator.number(1000, "number!{3.}")).toBe(false);
+
+  expect(validator.number(0.1, "number!{.2}")).toBe(true);
+  expect(validator.number(0.11, "number!{.2}")).toBe(true);
+  expect(validator.number(0.111, "number!{.2}")).toBe(false);
+
+  expect(validator.number(1.1, "number!{3.2}")).toBe(true);
+  expect(validator.number(11.11, "number!{3.2}")).toBe(true);
+  expect(validator.number(111.111, "number!{3.2}")).toBe(false);
+  expect(validator.number(11.111, "number!{3.2}")).toBe(false);
+  expect(validator.number(111.11, "number!{3.2}")).toBe(true);
 });
--- tests/string.test.ts
+++ tests/string.test.ts
@@ -2,27 +2,51 @@ import { expect, test } from "vitest";
 import { validator } from "../src/index";
 
 test("string type test", () => {
-  const data = [null, void 0, 1, true, "", "expression@validator.com"];
-  expect(data.map((value) => validator.string(value, "string?"))).toStrictEqual([true, true, false, false, false, true]);
-  expect(data.map((value) => validator.string(value, "string!"))).toStrictEqual([false, false, false, false, false, true]);
+  expect(validator.string(null, "string?")).toBe(true);
+  expect(validator.string(void 0, "string?")).toBe(true);
+  expect(validator.string(1, "string?")).toBe(false);
+  expect(validator.string("1", "string?")).toBe(true);
+
+  expect(validator.string(null, "string!")).toBe(false);
+  expect(validator.string(void 0, "string!")).toBe(false);
+  expect(validator.string(1, "string!")).toBe(false);
+  expect(validator.string("1", "string!")).toBe(true);
+
+  expect(validator.string(null, "string[]?")).toBe(true);
+  expect(validator.string(void 0, "string[]?")).toBe(true);
+  expect(validator.string([], "string[]?")).toBe(true);
+  expect(validator.string([1, 2, 3], "string[]?")).toBe(false);
+  expect(validator.string(["1", "2", "3"], "string[]?")).toBe(true);
+
+  expect(validator.string(null, "string[]!")).toBe(false);
+  expect(validator.string(void 0, "string[]!")).toBe(false);
+  expect(validator.string([], "string[]!")).toBe(true);
+  expect(validator.string([1, 2, 3], "string[]!")).toBe(false);
+  expect(validator.string(["1", "2", "3"], "string[]!")).toBe(true);
 });
 
-test("string length test", () => {
-  const data = "0123456789";
-  expect(validator.string(data, "string!{10~}")).toBe(true);
-  expect(validator.string(data, "string!{~10}")).toBe(true);
-  expect(validator.string(data, "string!{10~10}")).toBe(true);
+test("string length args test", () => {
+  expect(validator.string("a", "string!{2~}")).toBe(false);
+  expect(validator.string("ab", "string!{2~}")).toBe(true);
+  expect(validator.string("abc", "string!{2~}")).toBe(true);
+
+  expect(validator.string("a", "string!{~2}")).toBe(true);
+  expect(validator.string("ab", "string!{~2}")).toBe(true);
+  expect(validator.string("abc", "string!{~2}")).toBe(false);
+
+  expect(validator.string("a", "string!{2~2}")).toBe(false);
+  expect(validator.string("ab", "string!{2~2}")).toBe(true);
+  expect(validator.string("abc", "string!{2~2}")).toBe(false);
 });
 
-test("string pattern test", () => {
+test("string custom pattern args test", () => {
+  expect(validator.string("110", "string!{/^\\d+$/}")).toBe(true);
+});
+
+test("string pattern args test", () => {
   const [email, mobilePhone, telPhone, idNumber] = ["expression@validator.com", "13355556666", "020-12345678", "11010120000728790X"];
   expect(validator.string(email, "string!{email}")).toBe(true);
   expect(validator.string(mobilePhone, "string!{mobilePhone}")).toBe(true);
   expect(validator.string(telPhone, "string!{telPhone}")).toBe(true);
   expect(validator.string(idNumber, "string!{idNumber}")).toBe(true);
-});
-
-test("string custom pattern test", () => {
-  const data = "110";
-  expect(validator.string(data, "string!{/^\\d+$/}")).toBe(true);
 });
