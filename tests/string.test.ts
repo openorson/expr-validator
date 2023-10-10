d156ec79fcad0da45fa8f4e8b45dfea38fc13f43
DESKTOP-MD2B2EU\oyc <ouyangcheng@inmeng.net>
2023-10-10 11:36:00 +0800

    feat: 玩命开发中...

--- tests/string.test.ts
+++ tests/string.test.ts
@@ -1,7 +1,28 @@
 import { expect, test } from "vitest";
 import { validator } from "../src/index";
 
-test("string", () => {
-  const str: unknown = "orson@hotmail.com";
-  expect(validator.string(str, "string!{email}(字符)", { throw: true })).toBe(true);
+test("string type test", () => {
+  const data = [null, void 0, 1, true, "", "expression@validator.com"];
+  expect(data.map((value) => validator.string(value, "string?"))).toStrictEqual([true, true, false, false, false, true]);
+  expect(data.map((value) => validator.string(value, "string!"))).toStrictEqual([false, false, false, false, false, true]);
+});
+
+test("string length test", () => {
+  const data = "0123456789";
+  expect(validator.string(data, "string!{10~}")).toBe(true);
+  expect(validator.string(data, "string!{~10}")).toBe(true);
+  expect(validator.string(data, "string!{10~10}")).toBe(true);
+});
+
+test("string pattern test", () => {
+  const [email, mobilePhone, telPhone, idNumber] = ["expression@validator.com", "13355556666", "020-12345678", "11010120000728790X"];
+  expect(validator.string(email, "string!{email}")).toBe(true);
+  expect(validator.string(mobilePhone, "string!{mobilePhone}")).toBe(true);
+  expect(validator.string(telPhone, "string!{telPhone}")).toBe(true);
+  expect(validator.string(idNumber, "string!{idNumber}")).toBe(true);
+});
+
+test("string custom pattern test", () => {
+  const data = "110";
+  expect(validator.string(data, "string!{/^\\d+$/}")).toBe(true);
 });
